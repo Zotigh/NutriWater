a95793218232fe3892a78aed87d39d2ab64c7b45
Zotigh <lzotigh1@cnm.edu>
2022-09-01 21:08:56 -0600

    Added comments as well as added a dependency for annotations.

--- NutriWater/Models/SingleCalcualtion.cs
+++ NutriWater/Models/SingleCalcualtion.cs
@@ -1,4 +1,11 @@
-﻿namespace NutriWater.Models
+﻿/******************************************************
+*Programmer: Lance Zotigh(lzotigh1 @cnm.edu)
+*Purpose: A simple program that calculates the amount of chemicals you need to add to a bucket of water (4 Hydroponics)
+*******************************************************/
+
+using System.ComponentModel.DataAnnotations;
+
+namespace NutriWater.Models
 {
     public class SingleCalcualtion
     {
