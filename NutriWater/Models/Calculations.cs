17b47111027885a7021a9f16d31772cae47d2cbd
Zotigh <lzotigh1@cnm.edu>
2022-09-05 19:50:33 -0600

    Added some generics to the class Calculations which will hold a list of the calulations that are made.

--- NutriWater/Models/Calculations.cs
+++ NutriWater/Models/Calculations.cs
@@ -3,9 +3,14 @@
 *Purpose: A simple program that calculates the amount of chemicals you need to add to a bucket of water (4 Hydroponics)
 *******************************************************/
 
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+
 namespace NutriWater.Models
 {
     public class Calculations
     {
+        private List<>
     }
 }
